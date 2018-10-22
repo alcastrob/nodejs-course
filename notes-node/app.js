a761511374c486ddd56d993fdfaf8bafdafb21fe
Angel Castro <al.castro@gmail.com>
2018-10-22 20:39:08 +0200

    First use of module.exports

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,12 +1,16 @@
-console.log('Starting app');
+console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const notes = require('./notes');
 
-var user = os.userInfo();
+console.log(res);
 
-fs.appendFile('greetings.txt', `Hello ${user.username}!` , function(err){
-    if (err){
-        console.log('Unable to write to the file');
-    }
-});
+// var user = os.userInfo();
+
+// fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.` , function(err){
+//     if (err){
+//         console.log('Unable to write to the file');
+//     }
+// });
+
---
+++ notes-node/notes.js
@@ -0,0 +1,9 @@
+console.log('Starting notes.js');
+
+
+// module.exports.addNote = function () {
+module.exports.addNote = () => {
+    console.log('addNote');
+    return 'New note';
+};
+
