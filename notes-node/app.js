1349ceff005676130f6ec6723c60c6d884794188
Angel Castro <al.castro@gmail.com>
2018-10-22 21:31:35 +0200

    Simple example of use of lodash package.

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,16 +1,7 @@
 console.log('Starting app.js');
 
 const fs = require('fs');
-const os = require('os');
 const notes = require('./notes');
+const _ = require('lodash');
 
-console.log(res);
-
-// var user = os.userInfo();
-
-// fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.` , function(err){
-//     if (err){
-//         console.log('Unable to write to the file');
-//     }
-// });
 
--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,9 +1,7 @@
 console.log('Starting notes.js');
 
-
 // module.exports.addNote = function () {
 module.exports.addNote = () => {
     console.log('addNote');
     return 'New note';
 };
-
