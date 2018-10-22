c15d8a63e578c69408405d836ed6c88710f37863
Angel Castro <al.castro@gmail.com>
2018-10-22 22:20:21 +0200

    Added command line parameters. Use of the yargs package.

--- notes-node/app.js
+++ notes-node/app.js
@@ -3,5 +3,21 @@ console.log('Starting app.js');
 const fs = require('fs');
 const notes = require('./notes');
 const _ = require('lodash');
+const yargs = require('yargs');
 
+const argv = yargs.argv;
+var command = argv._[0];
+console.log('Command: ', command);
 
+if (command === 'add') {
+    console.log('Adding a new note');
+    notes.addNote(argv.title, argv.body);
+} else if (command === 'list') {
+    notes.getAll();
+} else if (command === 'read') {
+    notes.getNote(argv.title);
+} else if (command === 'remove') {
+    notes.removeNote(argv.title);
+} else {
+    console.log('Command not recognized');
+}
