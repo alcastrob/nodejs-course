c3880f05bb5763f6adaa2fffa379e07e90af2727
Angel Castro <al.castro@gmail.com>
2018-10-23 13:07:22 +0200

    Some refactor.

--- notes-node/app.js
+++ notes-node/app.js
@@ -10,8 +10,15 @@ var command = argv._[0];
 console.log('Command: ', command);
 
 if (command === 'add') {
-    console.log('Adding a new note');
-    notes.addNote(argv.title, argv.body);
+    var note = notes.addNote(argv.title, argv.body);
+    if (note){
+        console.log('Note created');
+        console.log('---');
+        console.log(`Title: ${note.title}`);
+        console.log(`Body: ${note.body}`);
+    } else {
+        console.log('Note title taken');
+    }
 } else if (command === 'list') {
     notes.getAll();
 } else if (command === 'read') {
