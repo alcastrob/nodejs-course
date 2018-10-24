b4bc19a4b411315852847f145f488abc28fbd77b
Angel Castro <al.castro@gmail.com>
2018-10-24 11:02:22 +0200

    Reading notes and fixing the debugging paths

--- notes-node/notes.js
+++ notes-node/notes.js
@@ -3,10 +3,10 @@ console.log('Starting notes.js');
 const fs = require('fs');
 
 var fetchNotes = () => {
-    try{
+    try {
         var notesString = fs.readFileSync('notes-data.json');
         return JSON.parse(notesString);
-    } catch(e){
+    } catch (e) {
         return [];
     }
 };
@@ -36,20 +36,29 @@ var getAll = () => {
 };
 
 var getNote = (title) => {
-    console.log('Getting the note ', title);
+    var notes = fetchNotes();
+    var filteredNotes = notes.filter((note) => note.title === title);
+    return filteredNotes[0];
 };
 
 var removeNote = (title) => {
     var notes = fetchNotes();
     var filteredNotes = notes.filter((note) => note.title !== title);
     saveNotes(filteredNotes);
-    
+
     return notes.length !== filteredNotes.length;
+}
+
+var logNote = (note) => {
+    console.log('---');
+    console.log(`Title: ${note.title}`);
+    console.log(`Body: ${note.body}`);
 }
 
 module.exports = {
     addNote,
     getAll,
     getNote,
-    removeNote
+    removeNote,
+    logNote
 };
